b5c47cae211a24cb35da85744914c9ac7a321e79
fanja.rajesson <fanja.rajesson@e2577382-de28-0410-a8ca-d9302b5a9fd5>
2007-03-05 19:36:17 +0000

    ne marche pas :( il ne trouve pas mon fichier txt!!!!

---
+++ graphwiz/src/graphWiz/ExplicationsDijkstra.java
@@ -0,0 +1,78 @@
+package graphWiz;
+
+import java.awt.GridLayout;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+
+import javax.swing.JFrame;
+import javax.swing.JList;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+
+public class ExplicationsDijkstra extends JPanel {
+
+	public static String Algorithme = "/GraphWiz/src/graphWiz/resources/Dijkstra.txt";
+	
+	public static String[] algo = new String[13];
+	
+	JList explicationsDijkstra;
+	
+	public ExplicationsDijkstra(){
+		super(new GridLayout(1,0));
+		
+		add(new JScrollPane(explicationsDijkstra));
+		//explicationsDijkstra.setFocusable(true);
+		
+		LireFichierAlgo();
+		
+	}
+
+	private void LireFichierAlgo() {
+		try {
+			RandomAccessFile raf = new RandomAccessFile(Algorithme, "r");
+			String ligne;
+			int i=0;
+			while ((ligne = raf.readLine()) != null ){
+				algo[i]=ligne;
+				i++;
+			}
+			explicationsDijkstra.setListData(algo);
+			System.out.println("algo chargé");
+		}  
+		catch (IOException e) {    
+			System.out.println("erreur dans: " + e);
+		}	
+		
+	}
+	
+	/**
+     * Create the GUI and show it.  For thread safety,
+     * this method should be invoked from the
+     * event-dispatching thread.
+     */
+    private static void createAndShowGUI() {
+        //Create and set up the window.
+        JFrame frame = new JFrame("Explications Dijkstra");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        //Create and set up the content pane.
+        ExplicationsDijkstra newContentPane = new ExplicationsDijkstra();
+        newContentPane.setOpaque(true); //content panes must be opaque
+        frame.setContentPane(newContentPane);
+
+        //Display the window.
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        //Schedule a job for the event-dispatching thread:
+        //creating and showing this application's GUI.
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                createAndShowGUI();
+            }
+        });
+    }
+	
+}
